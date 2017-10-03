65bedddf428f67ec517c8319c6bc1204e681f9b9
jk <strel.jk.93@gmail.com>
2017-10-03 23:51:26 +0300

    initial projects

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp')
+	sass = require('gulp-sass')
+	browserSync = require('browser-sync'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglifyjs'),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  cache = require('gulp-cache');
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/*.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app',
+		},
+		notify: false,
+	});
+});
+
+gulp.task('jsLib', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/popper.js/dist/popper.min.js',
+		'app/libs/bootstrap/dist/js/bootstrap.min.js',
+	])
+	.pipe(concat('libs.min.js'))
+	// .pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('watch', ['browser-sync', 'sass', 'jsLib'], function() {
+	gulp.watch('app/sass/**/*.+(scss|sass)', ['sass']);
+	gulp.watch('app/libs/bootstrap/scss/**/*.+(scss|sass)', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+gulp.task('img', function() {
+  return gulp.src('app/img/**/*')
+    .pipe(cache(imagemin({
+      interlaced: true,
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    })))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['clean', 'img', 'sass', 'jsLib'], function() {
+	var buildCss = gulp.src([
+			'app/css/main.css',
+			'app/css/libs.min.css',
+		])
+		.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'));
+
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('clear', function () {
+  return cache.clearAll();
+});
